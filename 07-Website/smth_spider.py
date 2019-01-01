cdf0202184d96e6c04641c61df41b2a85207622c
hezhen <hezhen112058@pwrd.com>
2019-01-01 23:27:29 +0800

    bug fix

--- 07-Website/smth_spider.py
+++ 07-Website/smth_spider.py
@@ -20,15 +20,15 @@ def get_article_posts(url):
 
 # Get all boards
 for i in range(0,10):
-    c = boards_crawler.get_pagination_content(i)
+    c = boards_crawler.get_board_of_section(i)
     boards += boards_crawler.get_board_list(c)
 
     # Get article list of board
     for board in boards:
         url = boards['board_url']
-        content = topics_crawler.get_pagination_content(url, 1)
-        articles = topics_crawler.get_article_list(content)
-        total_pages_of_board = topics_crawler.get_total_pages(content)
+        content = topics_crawler.get_content(url, 1)
+        articles = topics_crawler.get_post_list()
+        total_pages_of_board = topics_crawler.get_max_page()
         for article in articles:
             get_article_posts(article['article_url'])
         for i in range(2, total_pages_of_board+1):
